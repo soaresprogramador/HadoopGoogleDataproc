c2aae08c508415cbd0b5aa4ae33c9ea741b169f3
Paulo Soares <soaresprogramador@gmail.com>
2021-09-18 14:08:26 -0300

    Desafio final de Data Science do curso da DIO

--- contador.py
+++ contador.py
@@ -2,6 +2,6 @@ import sys
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySparkDioFinal")
-    words = sc.textFile("gs://dataproc-dio-final/livro.txt").flatMap(lambda line: line.split(" "))
+    words = sc.textFile("gs://'BUCKET'/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://dataproc-dio-final/resultado")
+    wordCounts.saveAsTextFile("gs://'BUCKET'/resultado")
